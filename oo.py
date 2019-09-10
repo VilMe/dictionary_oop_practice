5493197f00b2ca46ee7b3862b84c927590d92b2e
Vilma M <vilma.medrano00@gmail.com>
2019-09-09 22:51:29 -0700

    define student class

--- oo.py
+++ oo.py
@@ -1,1 +1,11 @@
 # Create your classes here
+class Student(object):
+    """docstring for Student"""
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+        
+        
